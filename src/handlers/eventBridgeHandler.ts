86b907f54ed45157d68442fae73f8608ca1b3e9b
aramis <aramis.ortega@bam.com.gt>
2025-05-28 12:41:24 -0500

    Fix handlers

--- src/handlers/eventBridgeHandler.ts
+++ src/handlers/eventBridgeHandler.ts
@@ -1,24 +1,20 @@
-import { UseCase, dependenciesType } from "@application/useCases/accountDebitCase";
-import { SqsQueue } from "@drivenAdapters/sqsQueue/sqsQueue";
-import { QUEUE_URL, THIRD_PARTY_URL } from "@utils/constants";
-import { ThridPartyApiAdapter } from "@drivenAdapters/thirdPartyApi/thirdPartyApi";
-import { EntityMysqlRepositoryFind } from '@drivenRepositories/account/repository/mysqlRepositoryFind';
-import { EntityMysqlRepositoryTransaction } from '@drivenRepositories/account/repository/mysqlRepositoryTransact';
-import { MyEntityMapper } from "@drivenMappers/myEntityMapper/myEntityMapper";
 import { EventBridgeEvent } from "aws-lambda";
-import { eventBridgeAdapter } from "@drivingAdapters/eventBridge/eventBridgeAdapter";
-import { ThirdPartyApiErrorMapper } from '@drivenAdapters/thirdPartyApi/thirdPartyErrorMapper/thirdPartyErrorMapper';
+import { QUEUE_URL, THIRD_PARTY_URL } from "@utils/constants";
+import { SqsQueue } from "@infrastructure/driven/adapters/sqsQueue/sqsQueue";
 import mySqlConnection from "@infrastructure/driven/database/mysqlConnection";
+import { accountDebitCase, dependenciesType } from "@application/useCases/accountDebitCase";
+import { ThridPartyApiAdapter } from "@infrastructure/driven/adapters/thirdPartyApi/thirdPartyApi";
+import { eventBridgeAdapter } from "@infrastructure/driving/adapters/eventBridge/eventBridgeAdapter";
+import { AccountMysqlRepository } from "@infrastructure/driven/repositories/account/repository/accountMysqlRepository";
+import { ThirdPartyApiErrorMapper } from '@infrastructure/driven/adapters/thirdPartyApi/thirdPartyErrorMapper/thirdPartyErrorMapper';
 
-const entityMapper = new MyEntityMapper();
 const dependencies: dependenciesType = {
     thirdPartyApi: new ThridPartyApiAdapter(THIRD_PARTY_URL, new ThirdPartyApiErrorMapper()),
     messageQueue: new SqsQueue(QUEUE_URL),
-    repositoryFind: new EntityMysqlRepositoryFind(entityMapper),
-    repositoryTransaction: new EntityMysqlRepositoryTransaction(entityMapper),
+    repository: new AccountMysqlRepository(mySqlConnection)
 }
 
-const useCase = new UseCase();
+const useCase = new accountDebitCase();
 
 export const handler = async (event: EventBridgeEvent<any,any>) => {
 
